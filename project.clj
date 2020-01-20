525a11c14329b57746b91bfbd02d6c060aabd7fb
cdzwm <495720919@qq.com>
2020-01-20 11:25:24 +0800

    Increase version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telnet_client "0.1.3"
+(defproject telnet_client "0.1.4"
   :description "Clojure wrap for TelnetClient"
   :url "https://github.com/cdzwm/telnet-client"
   :license {:name "MIT License"
