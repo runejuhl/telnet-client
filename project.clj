cbd9bc39f2990593e449440736a2a2b74c2a068e
canw <495720919@qq.com>
2020-01-20 11:29:33 +0800

    Fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telnet_client "0.1.4"
+(defproject telnet_client "0.1.5"
   :description "Clojure wrap for TelnetClient"
   :url "https://github.com/cdzwm/telnet-client"
   :license {:name "MIT License"
