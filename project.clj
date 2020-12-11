5b70c1735fd0d7adc73eafbc63a5b7a74deb40db
canw <canw@abc.com>
2020-12-11 10:04:32 +0800

    increase version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telnet_client "0.1.6"
+(defproject telnet_client "0.1.7"
   :description "Clojure wrap for TelnetClient"
   :url "https://github.com/cdzwm/telnet-client"
   :license {:name "MIT License"
