6c78ad58ab3937c6de881718286d63ec52443eb8
canw <canw@abc.com>
2020-12-25 15:26:06 +0800

    Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telnet-client "0.1.14"
+(defproject telnet-client "0.1.15"
   :description "Clojure wrap for TelnetClient"
   :url "https://github.com/cdzwm/telnet-client"
   :license {:name "MIT License"
