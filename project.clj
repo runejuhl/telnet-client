901a7baac99a30d3cdc33ebf1b5f3a3754effcef
Rune Juhl (Atea) <rune.juhl@atea.dk>
2023-12-06 23:21:24 +0100

    Downgrade Clojure dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject telnet-client "0.1.23"
+(defproject telnet-client "0.1.23-clj1.9"
   :description "Clojure wrap for TelnetClient"
   :url "https://github.com/cdzwm/telnet-client"
   :license {:name "MIT License"
             :url "https://www.mit-license.org/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [commons-net/commons-net "3.6"]]
   :aot [telnet-client.core]
   :profiles {
